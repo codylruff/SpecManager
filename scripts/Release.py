36f37907d037b4fb00a6cd1a9a79d8bb08cb7bb8
codylruff <39601024+codylruff@users.noreply.github.com>
2019-06-25 13:19:15 -0400

    Update Release.py starting directory

--- scripts/Release.py
+++ scripts/Release.py
@@ -121,7 +121,7 @@ def main():
     installer = args.installer
     
     # Initialize repo directory
-    repo_dir = r"C:/Users/cruff/source/SM - Final"
+    repo_dir = r"C:/Users/cruff/Documents/Projects/source/Spec-Manager”
 
     if not installer:
         # Create new release directory
