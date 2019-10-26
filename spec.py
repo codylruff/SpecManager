eed6f23806310dcb4cfa71296dd06e136ae4ae48
codylruff <39601024+codylruff@users.noreply.github.com>
2019-10-25 21:41:01 -0400

    Update spec.py

--- spec.py
+++ spec.py
@@ -4,3 +4,9 @@ class spec():
         self.material_id = material_id
         self.process_id = process_id
         self.properties = properties
+
+    def change_property(property_name, value):
+        self.properties[property_name] = value
+
+    def get_properties_json():
+        return 
